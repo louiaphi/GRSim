7061b966bc213c51debae81eff1beddababf9647
louiaphi <louiaphi01@gmail.com>
2026-01-29 22:11:39 +0100

    Fixed  ergosphere rendering and increased options to customize
    spend 45min Bugfixing stuff that wasn't wrong only to find that ergosphere is not written er-ghost-phere...
    Why do Compute Shaders ALWAYS run no matter how false they are written... One singular compiling mistake of something...

--- Assets/Camera/H-Heatmap.cs
+++ Assets/Camera/H-Heatmap.cs
@@ -16,16 +16,20 @@ public class ScalarFieldVisualizer : MonoBehaviour
     public float z;
 
     public float rim;
+    public float rim2;
 
     public bool enableEventHorizon;
     public bool enableErgosphere;
+
+    public Color Singularity;
+    public Color Portal;
+    public Color EventHorizon;
+    public Color Ergosphere;
 
     Texture2D texture;
 
     public float delT = 0;
 
-    public float min = 0;
-    public float max = 0;
     [Range(0.0f, 10.0f)]
     public float xgrob; //two sliders for better precision control
     [Range(0.0f, 1.0f)]
@@ -36,76 +40,6 @@ public class ScalarFieldVisualizer : MonoBehaviour
     {
 
     }
-
-    //float CalcH(float r, float z, float M, float a) //static strength of deviation from flat Minkowski Spacetime space (H = 0 -> Minkowski) at Horizon H = infinty
-    //{
-    //    float H = (M * Mathf.Pow(r, 3)) / (Mathf.Pow(r, 4) + Mathf.Pow(a, 2) * Mathf.Pow(z, 2));
-    //    return H;
-    //}
-    //
-    //public float CalcR2(Vector4 pos, float a)
-    //{
-    //    float rho2 = pos.y * pos.y + pos.z * pos.z + pos.w * pos.w;
-    //    return 0.5f * (rho2 - Mathf.Pow(a, 2) + Mathf.Sqrt(Mathf.Pow(rho2 - Mathf.Pow(a, 2), 2) + 4 * Mathf.Pow(a, 2) * Mathf.Pow(pos.w, 2)));
-    //}
-    //
-    //void GenerateTexture() // made by ChatGPT for debugging
-    //{
-    //    Color[] pixels = new Color[width * height];
-    //
-    //
-    //    float[,] values = new float[width, height];
-    //    for (int y = 0; y < height; y++)
-    //    {
-    //        for (int x = 0; x < width; x++)
-    //        {
-    //            float v = SampleField(x, y);
-    //            values[x, y] = v;
-    //            //min = Mathf.Min(min, v);
-    //            //max = Mathf.Max(max, v);
-    //        }
-    //    }
-    //
-    //    float range = max - min;
-    //
-    //    for (int y = 0; y < height; y++)
-    //    {
-    //        for (int x = 0; x < width; x++)
-    //        {
-    //            if (values[x, y] == -1)
-    //            {
-    //                pixels[y * width + x] = new Color(1, 0, 0, 1f);
-    //            }
-    //            else if (values[x, y] == -2)
-    //            {
-    //                pixels[y * width + x] = new Color(0, 1, 0, 1f);
-    //            }
-    //            float normalized = (values[x, y] - min) / range;
-    //            pixels[y * width + x] = new Color(normalized, normalized, normalized, 1f);
-    //        }
-    //    }
-    //
-    //    texture.SetPixels(pixels);
-    //    texture.Apply();
-    //}
-    //
-    //float SampleField(int x, int y)
-    //{
-    //    float r = Mathf.Sqrt(CalcR2(new Vector4(0, x - width/2, y-width/2, z), a));
-    //    if (r < 0.1f && r > -0.1f)
-    //    {
-    //        return -1;
-    //    }
-    //    if (r < -0.1f)
-    //    {
-    //        return -2;
-    //}
-    //    r *= scale;
-    //    float H = CalcH(r, z, M, a);
-    //    Debug.Log("R = " + Mathf.Sqrt(CalcR2(new Vector4(0, xpoint, 0, 0), a)));
-    //    Debug.Log("H = " + CalcH(CalcR2(new Vector4(0, xpoint, 0, 0), a), 0, M, a));
-    //    return H;
-    //}
 
     public ComputeShader fieldCS;
     RenderTexture target;
@@ -146,6 +80,11 @@ public class ScalarFieldVisualizer : MonoBehaviour
         fieldCS.SetFloat("EHRadius", RAtZ(CalcEHRadius(M, a), z));
         fieldCS.SetBool("enableEventHorizon", enableEventHorizon);
         fieldCS.SetBool("enableErgosphere", enableErgosphere);
+        fieldCS.SetVector("ColorSingularity", Singularity);
+        fieldCS.SetVector("ColorPortal", Portal);
+        fieldCS.SetVector("ColorEventHorizon", EventHorizon);
+        fieldCS.SetVector("ColorErgosphere", Ergosphere);
+        fieldCS.SetFloat("rim2", rim2);
         fieldCS.Dispatch(
             kernel,
             Mathf.CeilToInt(width / 8f),
@@ -170,7 +109,7 @@ public class ScalarFieldVisualizer : MonoBehaviour
             Dispatch();
 
             //GetComponent<Renderer>().material.mainTexture = texture;
-            //delT = 0;
+            delT = 0;
         }
 
     }
